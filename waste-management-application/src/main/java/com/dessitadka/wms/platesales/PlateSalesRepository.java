820ade21d6802b4a899267492811af4b00f6d245
Nrapendra Trivedi <trivajay259@gmail.com>
2024-11-10 19:26:58 +0100

    message: changed directory structure

--- waste-management-application/src/main/java/com/dessitadka/wms/platesales/PlateSalesRepository.java
+++ waste-management-application/src/main/java/com/dessitadka/wms/platesales/PlateSalesRepository.java
@@ -1,6 +1,5 @@
 package com.dessitadka.wms.platesales;
 
-import com.dessitadka.wms.groceryorders.GroceryOrder;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface PlateSalesRepository extends ListCrudRepository<PlateSales,Long> {
